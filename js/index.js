37360aacdef265421a207673861fb31a5f38c285
Brian Rainey <brainey22@gmail.com>
2020-12-18 23:53:01 -0600

    slide 1 hero css

--- js/index.js
+++ js/index.js
@@ -1,5 +1,32 @@
 $(document).ready(function(){
     // Activate Carousel
+    $("#heroCarousel").carousel("pause");
+
+    // Enable Carousel Indicators
+    $(".item1").click(function(){
+        $("#myCarousel").carousel(0);
+    });
+    $(".item2").click(function(){
+        $("#myCarousel").carousel(1);
+    });
+    $(".item3").click(function(){
+        $("#myCarousel").carousel(2);
+    });
+    $(".item4").click(function(){
+        $("#myCarousel").carousel(3);
+    });
+
+    // Enable Carousel Controls
+    $(".left").click(function(){
+        $("#heroCarousel").carousel("prev");
+    });
+    $(".right").click(function(){
+        $("#heroCarousel").carousel("next");
+    });
+});
+
+$(document).ready(function(){
+    // Activate Carousel
     $("#featuredHouseDealsCarousel").carousel("pause");
 
     // Enable Carousel Controls
